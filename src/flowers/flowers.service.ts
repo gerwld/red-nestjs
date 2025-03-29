91c44cab5a1b6b65d2aab2a1d7f47c68175a6452
PJaworski <pjaworski.dev@gmail.com>
2025-03-29 13:28:11 +0100

    Initial commit

---
+++ src/flowers/flowers.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FlowersService {
+    findAll() {
+        return [
+            {
+                name: "Rose",
+                color: "red",
+                price: 5
+            },
+            {
+                name: "Lily",
+                color: "white",
+                price: 5
+            },
+            {
+                name: "Tulip",
+                color: "yellow",
+                price: 7
+            },
+
+        ]
+    }
+}
